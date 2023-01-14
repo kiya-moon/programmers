c96a9dae5f0d949e8791f58190f9bee8bd712452
kiya-moon <kiyamoon@daum.net>
2023-01-14 09:42:02 +0900

    day8 - ing2

--- src/Day8.java
+++ src/Day8.java
@@ -7,6 +7,10 @@ public class Day8 {
         // 배열 자르기
         ArrayList arr = solution(new int[] {1, 2, 3, 4, 5}, 1, 3);
         System.out.println(arr);
+
+        // 외계행성의 나이
+        String age = solution(51);
+        System.out.println(age);
     }
 
     static ArrayList solution(int[] numbers, int num1, int num2){
@@ -21,6 +25,18 @@ public class Day8 {
 
         // IntStream 사용
 //        return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
+
+        return answer;
+    }
+
+    static String solution(int age){
+        String answer = "";
+        String[] arr = String.valueOf(age).split("");
+
+        for(int i = 0; i<arr.length; i++){
+            // 아스키코드를 이용
+            answer += (char)((Integer.parseInt(arr[i]) + 97));
+        }
 
         return answer;
     }
