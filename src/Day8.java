af4e4260a4376a914769f423929c2f3edb43af1c
kiya-moon <kiyamoon@daum.net>
2023-01-15 23:56:26 +0900

    day8 - ing3

--- src/Day8.java
+++ src/Day8.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
 public class Day8 {
@@ -11,6 +12,12 @@ public class Day8 {
         // 외계행성의 나이
         String age = solution(51);
         System.out.println(age);
+
+        // 진료 순서 정하기
+        int[] order = solution(new int[] {3, 76, 24});
+        for(int i : order){
+            System.out.println(order);
+        }
     }
 
     static ArrayList solution(int[] numbers, int num1, int num2){
@@ -36,6 +43,25 @@ public class Day8 {
         for(int i = 0; i<arr.length; i++){
             // 아스키코드를 이용
             answer += (char)((Integer.parseInt(arr[i]) + 97));
+        }
+
+        return answer;
+    }
+
+    static int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        int[] temp = Arrays.copyOf(emergency, emergency.length);
+        Arrays.sort(temp);
+
+        // emergency 배열을 받아서 temp 배열에 역정렬해서 넣음
+        // emergency 배열과 비교해서 answer의 인덱스 값 반환
+
+        for(int i = 0; i < emergency.length; i++){
+            for(int j = 0; j < temp.length; j++){
+                if(emergency[i] == temp[j]){
+                    answer[i] = j+1;
+                }
+            }
         }
 
         return answer;
