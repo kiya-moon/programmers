f7bfa9c7581259343a4b368278f62c607b96125d
kiya-moon <kiyamoon@daum.net>
2023-01-23 23:43:30 +0900

    day 12 ing2

--- src/Day12.java
+++ src/Day12.java
@@ -9,9 +9,15 @@ public class Day12 {
         System.out.println(removeVowel);
 
         // 문자열 정렬하기(1)
-        int[] sort = solution2("hi12392");
+        int[] sort = solution2("p2o4i8gj2");
         IntStream intStream = Arrays.stream(sort);
         intStream.forEach(i -> System.out.print(i + " "));
+
+        System.out.println();
+
+        // 숨어있는 숫자의 덧셈(1)
+        int sum = solution3("aAb1B2cC34oOp");
+        System.out.println(sum);
 
     }
 
@@ -26,8 +32,28 @@ public class Day12 {
 
     static int[] solution2(String my_string){
         int[] answer = {};
-        String[] temp = my_string.replaceAll("[^//d]", "").split("");
-        answer = Stream.of(temp).mapToInt(Integer::parseInt).sorted().toArray();
+        String temp = my_string.replaceAll("[^0-9]", "");
+        answer = Arrays.stream(temp.split("")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(answer);
+
+        // 한 줄로 가능한 거여써....
+//        answer =  Arrays.stream(my_string.replaceAll("[^0-9]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+
+        return answer;
+
+    }
+
+    static int solution3(String my_string){
+        int answer = 0;
+        int[] temp = {};
+
+        temp = Arrays.stream(my_string.replaceAll("[^0-9]", "").split(""))
+                .mapToInt(Integer::parseInt).toArray();
+
+        for(int i = 0; i < temp.length; i++){
+            answer += temp[i];
+        }
+
         return answer;
     }
 
