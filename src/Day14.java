c02f1cce73d4fbe9f2b73db7802748b714123894
kiya-moon <kiyamoon@daum.net>
2023-01-30 23:16:02 +0900

    day 14 end

--- src/Day14.java
+++ src/Day14.java
@@ -1,9 +1,20 @@
 import java.util.Arrays;
-import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day14 {
     public static void main(String[] args) {
+        // 가까운 수
         System.out.println(solution(new int[]{3, 30, 10}, 20));
+
+        // 369게임
+        System.out.println(solution(29423));
+
+        // 암호 해독
+        System.out.println(solution("pfqallllabwaoclk", 2));
+
+        // 대문자와 소문자
+        System.out.println(solution("abCdEfghIJ"));
     }
 
     static int solution(int[] array, int n){
@@ -32,5 +43,34 @@ public class Day14 {
         answer = array[0];
 
         return answer;
+    }
+
+    static int solution(int order){
+        return (int) Stream.of(String.valueOf(order).split("")).filter(i -> i.equals("3") || i.equals("6") || i.equals("9")).count();
+    }
+
+    static String solution(String cipher, int code){
+        String answer = "";
+        char[] arr = cipher.toCharArray();
+        for(int i = code-1; i < arr.length; i += code){
+            answer += arr[i];
+        }
+        return answer;
+    }
+
+    static String solution(String my_string){
+        String answer = "";
+        for(char c : my_string.toCharArray()){
+            if(Character.isUpperCase(c)){
+                answer += String.valueOf(c).toLowerCase();
+            } else{
+                answer += String.valueOf(c).toUpperCase();
+            }
+        }
+        return answer;
+
+        // 한 줄 리턴
+//        return my_string.chars().mapToObj(c -> String.valueOf((char)
+//                (Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c)))).collect(Collectors.joining());
     }
 }
