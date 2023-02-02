64ba619a9b8c78a812a7c97cbb886082f1dde4d8
kiya-moon <kiyamoon@daum.net>
2023-02-02 22:24:41 +0900

    day 15 ing

--- src/Day15.java
+++ src/Day15.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Day15 {
     public static void main(String[] args) {
         // 영어가 싫어요
         System.out.println(solution("onefourzerosixseven"));
+
+        // 인덱스 바꾸기
+        System.out.println(solution("I love you", 3, 6));
     }
 
     static long solution(String numbers){
@@ -28,5 +37,24 @@ public class Day15 {
         answer = Long.parseLong(numbers);
 
         return answer;
+    }
+
+    static String solution(String my_string, int num1, int num2){
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < my_string.length(); i++){
+            if(i == num1){
+                answer.append(my_string.charAt(num2));
+            } else if (i == num2) {
+                answer.append(my_string.charAt(num1));
+            } else {
+                answer.append(my_string.charAt(i));
+            }
+        }
+        return answer.toString();
+
+        // Collection.swap()
+//        List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList());
+//        Collections.swap(list, num1, num2);
+//        return String.join("", list);
     }
 }
