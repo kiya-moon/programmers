061ef6097b7005ca70f3bee93746fd52c46ae7a8
kiya-moon <kiyamoon@daum.net>
2023-02-11 23:27:47 +0900

    day 16 -ing

---
+++ src/Day16.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Day16 {
+    public static void main(String[] args) {
+        // 편지
+        System.out.println(solution("I love you~"));
+    }
+
+    public static int solution(String message){
+        return message.length() * 2;
+    }
+
+    public static int[] solution(int[] array){
+        int[] answer = {};
+        return answer;
+    }
+}
