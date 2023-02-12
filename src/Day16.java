9ee0b8f7063541944256bedd71b1a0f1a5dd4e92
kiya-moon <kiyamoon@daum.net>
2023-02-12 23:54:00 +0900

    day 16 -ing2

--- src/Day16.java
+++ src/Day16.java
@@ -1,10 +1,15 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Day16 {
     public static void main(String[] args) {
         // 편지
         System.out.println(solution("I love you~"));
+
+        // 가장 큰 수 찾기
+        int[] answer = solution(new int[] {1, 8, 3});
+        for(int i : answer){
+            System.out.println(i);
+        }
     }
 
     public static int solution(String message){
@@ -12,7 +17,14 @@ public class Day16 {
     }
 
     public static int[] solution(int[] array){
-        int[] answer = {};
+        int max = Arrays.stream(array).max().getAsInt();
+        int idx = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == max){
+                idx = i;
+            }
+        }
+        int[] answer = {max, idx};
         return answer;
     }
 }
