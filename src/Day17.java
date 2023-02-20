f047983a6e72a3702c1bf390c660f8780d1de21a
kiya-moon <kiyamoon@daum.net>
2023-02-20 23:39:40 +0900

    day 17 -ing2

--- src/Day17.java
+++ src/Day17.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Day17 {
     public static void main(String[] args) {
+        //
         System.out.println(solution(29183, 0));
+
+        // n의 배수 고르기
+        Arrays.stream(solution(3, new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12})).forEach(i -> System.out.print(i + " "));
     }
 
     public static int solution(int num, int k){
@@ -23,5 +29,8 @@ public class Day17 {
 //        return ("-" + num).indexOf(String.valueOf(k));
     }
 
-
+    public static int[] solution(int n, int[] numlist){
+        return Arrays.stream(numlist).boxed().filter(i -> i % n == 0).mapToInt(Integer::intValue).toArray();
+        // return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+    }
 }
