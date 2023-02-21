d6a09ff90008096a0db5209f064d7fdadbf19382
kiya-moon <kiyamoon@daum.net>
2023-02-21 23:53:53 +0900

    day 17 -end

--- src/Day17.java
+++ src/Day17.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Day17 {
     public static void main(String[] args) {
-        //
+        // 숫자 찾기
         System.out.println(solution(29183, 0));
 
         // n의 배수 고르기
         Arrays.stream(solution(3, new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12})).forEach(i -> System.out.print(i + " "));
+
+        System.out.println();
+
+        // 자릿수 더하기
+        System.out.println(solution(930211));
+
+        // OX 퀴즈
+        Arrays.stream(solution(new String[]{"-3 - 3 = -6"})).forEach(i -> System.out.print(i + " "));
     }
 
     public static int solution(int num, int k){
@@ -32,5 +43,33 @@ public class Day17 {
     public static int[] solution(int n, int[] numlist){
         return Arrays.stream(numlist).boxed().filter(i -> i % n == 0).mapToInt(Integer::intValue).toArray();
         // return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+    }
+
+    public static int solution(int n){
+        return Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).sum();
+
+        // while문으로 풀이 할 때
+//        int answer = 0;
+//        while (n > 0){
+//            answer += n % 10;
+//            n /= 10;
+//        }
+//        return answer;
+    }
+
+    public static String[] solution(String[] quiz){
+        String[] answer = {};
+        List<String> list = new ArrayList<>();
+        String[] temp = {};
+        for(String s : quiz){
+            temp = s.split(" ");
+            if(temp[1].equals("-")){
+                list.add((Integer.parseInt(temp[0])) - Integer.parseInt(temp[2]) == Integer.parseInt(temp[4]) ? "O" : "X");
+            } else {
+                list.add((Integer.parseInt(temp[0])) + Integer.parseInt(temp[2]) == Integer.parseInt(temp[4]) ? "O" : "X");
+            }
+        }
+        answer = list.toArray(new String[list.size()]);
+        return answer;
     }
 }
