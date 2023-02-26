3c6b8c1394778b6988fa2ceeda86fda0b7f09def
kiya-moon <kiyamoon@daum.net>
2023-02-26 23:55:49 +0900

    day 19 - ing

---
+++ src/Day19.java
@@ -0,0 +1,13 @@
+public class Day19 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[] array){
+        int answer = 0;
+        // 배열에 7이 총 몇 개 있는지...
+
+        return answer;
+
+    }
+}
