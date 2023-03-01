04575449a015b3c6e4411b1eba1969bc29365a87
kiya-moon <kiyamoon@daum.net>
2023-03-01 23:57:30 +0900

    day 19 - ing3

--- src/Day19.java
+++ src/Day19.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day19 {
     public static void main(String[] args) {
         // 7의 개수
-        System.out.println(solution(new int[] {7, 77, 17}));
+        System.out.println(solution(new int[] {10, 29}));
+
+        // 잘라서 배열로 저장하기
+        System.out.println(solution("abc1Addfggg4556b", 6));
     }
 
     public static int solution(int[] array){
-        int answer = 0;
-        // 배열에 7이 총 몇 개 있는지...
-        String str = Arrays.stream(Arrays.stream(array).mapToObj(String::valueOf).toArray(String[]::new)).collect(Collectors.joining());
+        return (int)Arrays.stream(Arrays.stream(array).mapToObj(String::valueOf).collect(Collectors.joining())
+                .split("")).filter(i -> i.equals("7")).count();
+    }
 
-        return (int)Arrays.stream(str.split("")).filter(i -> i == "7").count();
-
+    public static String[] solution(String my_str, int n){
+        List list = new ArrayList<String>();
+        for(int i = 0; i <= my_str.length(); i+=n){
+            list.add(my_str.substring(i, i+n));
+            String a = list.get(0).toString();
+        }
+        return new String[list.size()];
     }
 }
